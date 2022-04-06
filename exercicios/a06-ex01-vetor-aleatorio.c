f42b1ab1f65575469a326ec5f54b6df835fb17e1
AlexandreNP9-university <peder@alunos.utfpr.edu.br>
2022-04-06 16:24:20 -0300

    aula 6abr

---
+++ exercicios/a06-ex01-vetor-aleatorio.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+//com calloc
+int* criaVetor1(int tam){
+    int* v = (int*)calloc(tam, sizeof(int));
+    for (int i=0; i < tam; i++){
+        v[i] = rand();
+    }
+
+    return v;
+}
+
+//com malloc
+void criaVetor2(int tam, int* endereco){
+    for (int i=0; i < tam; i++){
+        endereco[i] = rand();
+    }
+}
+
+void imprimeVetor(int* vetor, int tam){
+    printf("[");
+    for (int i=0; i < tam; i++){
+        printf("%d", vetor[i]);
+        if(i < tam-1) printf(",");
+    }
+    printf("]\n");
+}
+
+int main(){
+    
+    int* v1 = criaVetor1(5);
+    imprimeVetor(v1, 5);
+
+    int* v2 = (int*) malloc (5 * sizeof(int));
+    criaVetor2(5, v2);
+    imprimeVetor(v2, 5);
+
+
+    return 0;
+}
